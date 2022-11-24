07bc51905525c7565538eb88bc0b9dd9eb94b519
MelvinTomezak <melvin.tomezak@etu.univ-amu.fr>
2022-11-24 11:17:54 +0100

    Avancement sur les différentes classes

--- src/Jeu.java
+++ src/Jeu.java
@@ -1,3 +1,7 @@
 public class Jeu extends Perso {
 
+    public Jeu(String nom, String attkspe, String transformation) {
+        super(nom, attkspe, transformation);
+    }
+
 }
--- src/Perso.java
+++ src/Perso.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
 public class Perso {
-    private String nom;
+    private Scanner nom;
     private String Attkspe;
-    private String
+    private String transformation;
+    Scanner saisienom = new Scanner(System.in);
+    public Perso(String nom, String attkspe, String transformation) {
+        System.out.println("Entrez un blaze");
+        this.nom = saisienom;
+        Attkspe = attkspe;
+        this.transformation = transformation;
+    }
+
+    public Scanner getNom() {
+        return nom;
+    }
+
+    public String getAttkspe() {
+        return Attkspe;
+    }
+
+    public void setAttkspe(String attkspe) {
+        Attkspe = attkspe;
+    }
+
+    public String getTransformation() {
+        return transformation;
+    }
+
+    public void setTransformation(String transformation) {
+        this.transformation = transformation;
+    }
 }
